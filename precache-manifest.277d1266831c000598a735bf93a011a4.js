6177ef09c2b8cd5c7bf6e85fc7b7aa9493aade90
Hurricane0 <nikitadv777@gmail.com>
2020-02-18 23:14:56 +0200

    Updates

---
+++ precache-manifest.277d1266831c000598a735bf93a011a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b164b62d5c4de85b2c960a824ac2acec",
+    "url": "/Alena-Nails/index.html"
+  },
+  {
+    "revision": "52ec6feae12852274652",
+    "url": "/Alena-Nails/static/css/main.bbcc3eeb.chunk.css"
+  },
+  {
+    "revision": "1bca8b9c61873dc7cf3e",
+    "url": "/Alena-Nails/static/js/2.788f3459.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Alena-Nails/static/js/2.788f3459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52ec6feae12852274652",
+    "url": "/Alena-Nails/static/js/main.5f9732f3.chunk.js"
+  },
+  {
+    "revision": "e67ae4d3979ad6df4e32",
+    "url": "/Alena-Nails/static/js/runtime-main.69545e1f.js"
+  },
+  {
+    "revision": "d7795ff3447b77f3bfca307023e1d2a6",
+    "url": "/Alena-Nails/static/media/BigLogo.d7795ff3.svg"
+  },
+  {
+    "revision": "f60b6f2791d84126dc76d4fe431d6295",
+    "url": "/Alena-Nails/static/media/Logo_Nails_SVG.f60b6f27.svg"
+  }
+]);
